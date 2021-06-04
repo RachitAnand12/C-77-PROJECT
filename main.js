022bd7b36c13abc35da5a9d6aea8762a1440d37c
RachitAnand12 <78621216+RachitAnand12@users.noreply.github.com>
2021-06-04 11:59:41 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+console.log("The Console Project");
+
+var firstNAME = "Rachit";
+var lastNAME = "Anand";
+
+var fullNAME = firstNAME.concat(lastNAME);
+console.log(fullNAME);
+
+var NUMBER1 = 144;
+var NUMBER2 = 12;
+
+var addition = NUMBER1 + NUMBER2 ;
+console.log(addition);
+
+var Subtraction = NUMBER1 - NUMBER2 ;
+console.log(Subtraction);
+
+var Multiplication = NUMBER1 * NUMBER2 ;
+console.log(Multiplication);
+
+var Division = NUMBER1 / NUMBER2 ;
+console.log(Division);
